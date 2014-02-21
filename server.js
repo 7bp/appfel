b0acdcb13b53586e090b0050c131fec3776c4af1
Benjamin Philipp <bphilipp@dimensional.de>
2014-02-21 13:35:31 +0100

    Change structure

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-var bplist = require('./bplistparser.js');
-var Q = require('q');
+var formidable = require('formidable'),
+    http = require('http'),
+    util = require('util'),
+    bplist = require('./lib/bplistparser.js')
+    Q = require('q');
 
 var bplist_parseFile = function(file, callback) {
   var deferred = Q.defer();
@@ -10,7 +13,31 @@ var bplist_parseFile = function(file, callback) {
   return deferred.promise.nodeify(callback);
 };
 
-bplist_parseFile('file.plist', function(err, obj) {
-  if (err) throw err;
-  console.log(JSON.stringify(obj));
-});
+http.createServer(function(req, res) {
+  if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+    // parse a file upload
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function(err, fields, files) {
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('received upload:\n\n');
+      bplist_parseFile(files.upload.path, function(err, obj) {
+        if (err) throw err;
+        res.end(util.inspect(obj));
+      });
+      //res.end(util.inspect({fields: fields, files: files}));
+    });
+
+    return;
+  }
+
+  // show a file upload form
+  res.writeHead(200, {'content-type': 'text/html'});
+  res.end(
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<input type="text" name="title"><br>'+
+    '<input type="file" name="upload" multiple="multiple"><br>'+
+    '<input type="submit" value="Upload">'+
+    '</form>'
+  );
+}).listen(8080);
